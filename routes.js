a96cb9137aed7a6383e3d6f3b8f6a5d996e0dca1
jacksonbates <malgalin@gmail.com>
2016-11-17 11:23:32 +1100

    adds server and simple routes file

---
+++ routes.js
@@ -0,0 +1,7 @@
+var router = require( 'express' ).Router;
+
+router.get( '/', ( req, res ) => {
+  res.send( 'Route active' );
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require( 'express' );
+var app = express();
+var mongodb = require( 'mongodb' );
+var mongo = mongodb.MongoClient;
+
+var mongoUserPsw = process.env.MONGO_USER_PSW;
+var url = 'mongodb://' + monogUserPsw + '@ds155737.mlab.com:55737/pair-with-me';
+
+mongo.connect( url, function( err, db ) {
+  if ( err ) {
+    console.log( 'Error: Could not connect to DB' );
+  } else {
+    console.log( 'Success: Connected to DB' );
+    
+    app.use( function( req, res, next) {
+      req.db = db;
+      next();
+    });
+
+    app.use( '/api/v1', require( './routes' ));
+  }
+
+  app.set( 'port', ( process.env.PORT || 5000 ));
+  
+  app.listen( app.get( 'port' ), function() {
+    console.log( 'Node app is running on port ', app.get( 'port' ) );
+  });
+});

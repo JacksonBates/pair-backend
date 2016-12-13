3fa99b7a51436c3480bcd354f3a3cf72ab05658b
jacksonbates <malgalin@gmail.com>
2016-12-13 11:50:10 +1100

    cleans up server.js and renames v1 routes

--- server.js
+++ server.js
@@ -26,29 +26,10 @@ if ( !process.env.NODE_ENV === 'test' ) {
 // Disable x-powered-by header which shows what software server is running (express);
 app.disable('x-powered-by');
 
-// Add headers
-// Source: http://stackoverflow.com/questions/18310394/no-access-control-allow-origin-node-apache-port-issue
-app.use(function (req, res, next) {
-
-  // Currently allows access from any origin
-  res.setHeader('Access-Control-Allow-Origin', 'http://pairboard.surge.sh');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use( require( './middleware/access-control-header' ));
 
 // Middleware for routes
-app.use( '/api/v1', require( './routes' ));
+app.use( '/api/v1', require( './api-v1' ));
 
 app.set( 'port', ( process.env.PORT || 3001 ));
 server.listen( app.get( 'port' ), function() {
@@ -66,13 +47,6 @@ io.sockets.on( 'connection', socket => {
       console.log( 'deleting: ', data);
       socket.broadcast.emit( 'delete', { delete: data });
     });
-    // We're connected to someone now. Let's listen for events from them
-    // socket.on('some event', function(data) {
-    //     // We've received some data. Let's just log it
-    //     console.log(data);
-    //     // Now let's reply
-    //     socket.emit('event', {some: "data"});
-    // });
 });
 
 module.exports = app;
